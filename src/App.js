d5fc2330b3286207b36248b9b4faf024c6e5ac7a
yuniverse96 <easy9607@gmail.com>
2025-07-09 14:25:16 +0900

    #초기세팅

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        리액트 초기세팅.
+      </header>
+    </div>
+  );
+}
+
+export default App;
