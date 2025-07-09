cd6fecc29a892b3f4bfcc150830b4e7ed8813b6d
yuniverse96 <easy9607@gmail.com>
2025-07-09 15:03:51 +0900

    # 레이아웃 기초 세팅

---
+++ src/Main.jsx
@@ -0,0 +1,47 @@
+
+import './components/style/Common.css';
+import Header from './components/Header';
+import ScrollSection from './components/ScrollSection';
+import Section from './components/Section';
+import Cover from './components/Cover';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+
+function Main() {
+  return (
+    <>
+    <Header />
+    <main>
+      <ScrollSection id="cover">
+        <Section title="Cover">
+          <Cover />
+        </Section>
+      </ScrollSection>
+      
+      <ScrollSection id="about">
+        <Section title="About">
+          <About />
+        </Section>
+      </ScrollSection>
+
+      <ScrollSection id="portfolio">
+        <Section title="Portfolio">
+          <Portfolio />
+        </Section>
+      </ScrollSection>
+
+      <ScrollSection id="contact">
+        <Section title="Contact">
+          <Contact />
+        </Section>
+      </ScrollSection>
+    </main>
+  </>
+  );
+}
+
+export default Main;
+
+
+
